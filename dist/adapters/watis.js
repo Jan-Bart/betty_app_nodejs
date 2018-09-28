ff68a4228731b91054a11ac297d8e374a2ff3900
Maarten Bogaert <mabogie@gmail.com>
2018-09-28 15:33:25 +0200

    dist ...

--- dist/adapters/watis.js
+++ dist/adapters/watis.js
@@ -13,6 +13,10 @@ var _lodash = require('lodash');
 
 var _lodash2 = _interopRequireDefault(_lodash);
 
+var _request = require('request');
+
+var _request2 = _interopRequireDefault(_request);
+
 var _betty = require('../betty');
 
 var _betty2 = _interopRequireDefault(_betty);
@@ -25,7 +29,7 @@ const sorry = ['Het spijt me {user}, ik heb geen idee.', 'Hoe moet ik dat nu wet
 
 const matches = ['wat is', 'waddis', 'wuk es', 'what is', 'what s', 'waddis'];
 
-const descriptions = [['acpaas', '*ACPaaS* \n_Antwerp City Platform as a Service_'], ['alm', '*ALM* \n_Application lifecycle management_'], ['bff', '*BFF* \n_Backend For Frontend_'], ['amap', '*AMAP* \n_Algemene meldingen app_'], ['braas', '*BRaaS* \n_Business Roles as a Service_'], ['bza', '*BZA* \n_Brandweer Zone Antwerpen_'], ['crs', '*CRS* \n_Centraal Referentie Systeem_'], ['daas', '*DAAS* \n_Digipolis Antwerp Application Stack_'], ['dl', '*DL* \n_District & Loketwerking_'], ['dxp', '*DXP* \n_Digital eXperience Platform_'], ['fmo', '*FMO* \n_Facility Management Openbaar domein_'], ['gdp', '*GDP* \n_Generiek Dossier Platform_'], ['gfp', '*GFP* \n_SAP onderdeel (verkopen)_'], ['gmp', '*GMP* \n_Generiek Meldingen Platform_'], ['gvp', '*GVP* \n_Generiek Verkoop Platform_'], ['iam', '*IAM* \n_Identity & Access Management_'], ['idc', '*IDC* \n_Uniek id van CRS-O (mapt 1-op-1 met KBO-nummer)_'], ['klm', '*KLM* \n_Klanten Management_ :airplane:'], ['love', ':heart: https://www.youtube.com/watch?v=HEXWRTEbj1I'], ['lpa', '*LPA* \n_Lokale Politie Antwerpen_ :female-police-officer:'], ['lrs', '*LRS* \n_Lokaal Referentie Systeem_'], ['mdm', '*MDM* \n_Mobile device management_'], ['os', '*OS* \n_Ondernemen & Stadsmarketing_\n_Operating System_'], ['sc', '*SC* \n_Strategische Coördinatie_'], ['um', '*UM* \n_User Management_\n Bereikbaar op https://um.antwerpen.be'], ['ume', '*UME* \n_User Management Engine_'], ['wbs', '*WBS* \n_Work-Breakdown Structure_'], ['wcm', '*WCM* \n_Web Content Management (system)_\n Meer info bij het ALM-team.']];
+const descriptions = [['acpaas', '*ACPaaS* \n_Antwerp City Platform as a Service_'], ['alm', '*ALM* \n_Application lifecycle management_'], ['bff', '*BFF* \n_Backend For Frontend_'], ['amap', '*AMAP* \n_Algemene meldingen app_'], ['braas', '*BRaaS* \n_Business Roles as a Service_'], ['bza', '*BZA* \n_Brandweer Zone Antwerpen_'], ['crs', '*CRS* \n_Centraal Referentie Systeem_'], ['daas', '*DAAS* \n_Digipolis Antwerp Application Stack_'], ['dl', '*DL* \n_District & Loketwerking_'], ['dxp', '*DXP* \n_Digital eXperience Platform_'], ['fmo', '*FMO* \n_Facility Management Openbaar domein_'], ['gdp', '*GDP* \n_Generiek Dossier Platform_'], ['gfp', '*GFP* \n_SAP onderdeel (verkopen)_'], ['gmp', '*GMP* \n_Generiek Meldingen Platform_'], ['gvp', '*GVP* \n_Generiek Verkoop Platform_'], ['iam', '*IAM* \n_Identity & Access Management_'], ['idc', '*IDC* \n_Uniek id van CRS-O (mapt 1-op-1 met KBO-nummer)_'], ['klm', '*KLM* \n_Klanten Management_ :airplane:'], ['love', ':heart: https://www.youtube.com/watch?v=HEXWRTEbj1I'], ['lpa', '*LPA* \n_Lokale Politie Antwerpen_ :female-police-officer:'], ['lrs', '*LRS* \n_Lokaal Referentie Systeem_'], ['mdm', '*MDM* \n_Mobile device management_'], ['os', '*OS* \n_Ondernemen & Stadsmarketing_\n_Operating System_'], ['sc', '*SC* \n_Strategische Coördinatie_'], ['solipsisme', '*Solipsisme* \n_Solipsisme is het geloof of de filosofie dat er maar een enkel bewustzijn bestaat: dat van de waarnemer. Het hele universum en alle andere personen waarmee gecommuniceerd wordt bestaan slechts in de geest van de waarnemer._'], ['um', '*UM* \n_User Management_\n Bereikbaar op https://um.antwerpen.be'], ['ume', '*UME* \n_User Management Engine_'], ['wbs', '*WBS* \n_Work-Breakdown Structure_'], ['wcm', '*WCM* \n_Web Content Management (system)_\n Meer info bij het ALM-team.']];
 
 function findMatch(string) {
   let found = false;
@@ -64,16 +68,42 @@ function handle(event) {
     };
     _betty2.default.emit('response', resp);
   } else {
-    _betty2.default.getSlackUser(event.user).then(user => {
-      console.log(user);
-      const resp = {
-        message: sorry[Math.floor(Math.random() * sorry.length)].replace('{user}', user.user.profile.first_name),
-        channel: event.channel
-      };
-      _betty2.default.emit('response', resp);
-    }).catch(err => {
-      console.log(err);
+    // try urban dictionary
+    _request2.default.get(`http://api.urbandictionary.com/v0/define?term=${match}`, (err, data, body) => {
+      if (err) {
+        _betty2.default.getSlackUser(event.user).then(user => {
+          const resp = {
+            message: sorry[Math.floor(Math.random() * sorry.length)].replace('{user}', user.user.profile.first_name),
+            channel: event.channel
+          };
+          _betty2.default.emit('response', resp);
+        });
+      } else {
+        const b = JSON.parse(body);
+        if (b.list.length > 0) {
+          const attachments = {
+            mrkdwn_in: ['text', 'pretext'],
+            text: b.list[0].definition,
+            title: b.list[0].word,
+            title_link: b.list[0].permalink
+          };
+          const resp = {
+            message: b.list[0].definition,
+            channel: event.channel,
+            attachments
+          };
+
+          _betty2.default.emit('response', resp);
+        } else {
+          _betty2.default.getSlackUser(event.user).then(user => {
+            const resp = {
+              message: sorry[Math.floor(Math.random() * sorry.length)].replace('{user}', user.user.profile.first_name),
+              channel: event.channel
+            };
+            _betty2.default.emit('response', resp);
+          });
+        }
+      }
     });
   }
-  return true;
 }
