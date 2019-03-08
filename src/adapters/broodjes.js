41e95e6f1c16ee6b81455fb194470a20290438c4
Stefan Pante <me@stefanpante.com>
2019-03-08 10:39:01 +0100

    change betty command

--- src/adapters/broodjes.js
+++ src/adapters/broodjes.js
@@ -12,11 +12,11 @@ async function createBroodjeslijstAttachment() {
   if (data.length === 0) {
     return null;
   }
-  let attachment = '';
 
-  data.forEach((element) => {
-    attachment += `Broodje ${element.broodje} - ${element.userName}\n`;
-  });
+  const attachment = data
+    .map(element => `${element.broodje} - ${element.userName}`)
+    .join('\n');
+
   const attachmentData = {
     mrkdwn_in: ['text', 'pretext'],
     text: attachment,
@@ -76,22 +76,22 @@ async function broodjeHalen(event) {
   // first check if no chinese has been assigned today
   let chineseUser = await Broodje.findOne({ chinese: true, createdAt: { $gte: moment().startOf('day') } });
   if (chineseUser) {
-    broodjesReaction(`${chineseUser.userName} haalt vandaag de broodjes!`, event, null);
-  } else {
-    try {
-      const volunteers = await getWeights();
-      const weights = volunteers.map(user => user.chance);
-      // now we got a workforce, let's get our chinese volunteer
-      const chineesIndex = weightedRandom(weights);
-      const chinees = volunteers[chineesIndex].user;
-      chineseUser = await Broodje.findOne({ createdAt: { $gte: moment().startOf('day') }, userId: chinees.id });
-      chineseUser.chinese = true;
-      await chineseUser.save();
-      broodjesReaction(`${chineseUser.userName} haalt vandaag de broodjes!`, event, null);
-    } catch (e) {
-      console.log(e);
-      broodjesReaction('Er is een probleem met het bepalen van de chinese vrijwilliger!', event, null);
-    }
+    return broodjesReaction(`${chineseUser.userName} haalt vandaag de bestelling!`, event, null);
+  }
+
+  try {
+    const volunteers = await getWeights();
+    const weights = volunteers.map(user => user.chance);
+    // now we got a workforce, let's get our chinese volunteer
+    const chineesIndex = weightedRandom(weights);
+    const chinees = volunteers[chineesIndex].user;
+    chineseUser = await Broodje.findOne({ createdAt: { $gte: moment().startOf('day') }, userId: chinees.id });
+    chineseUser.chinese = true;
+    await chineseUser.save();
+    broodjesReaction(`${chineseUser.userName} haalt vandaag de bestelling!`, event, null);
+  } catch (e) {
+    console.log(e);
+    broodjesReaction('Er is een probleem met het bepalen van de chinese vrijwilliger!', event, null);
   }
 }
 
@@ -109,7 +109,7 @@ async function broodjeHalenSet(event) {
   const user = await Betty.getSlackUser(event.user);
   const newChineseUser = await Broodje.findOne({ userId: user.user.id, createdAt: { $gte: moment().startOf('day') } });
   if (!newChineseUser) {
-    broodjesReaction('Je moet een broodje besteld hebben om te kunnen gaan halen', event, null);
+    broodjesReaction('Je moet iets besteld hebben om te kunnen gaan halen', event, null);
   }
   // als user deelnemer is
   const chineseUser = await Broodje.findOne({ chinese: true, createdAt: { $gte: moment().startOf('day') } });
@@ -119,18 +119,18 @@ async function broodjeHalenSet(event) {
   }
   newChineseUser.chinese = true;
   await newChineseUser.save();
-  broodjesReaction(`[UPDATE] ${newChineseUser.userName} haalt vandaag de broodjes!`, event, null);
+  broodjesReaction(`[UPDATE] ${newChineseUser.userName} haalt vandaag de bestelling!`, event, null);
 }
 
 
 async function broodjesStats(event) {
   const countFrom = new Date((new Date().getTime() - (30 * 24 * 60 * 60 * 1000)));
   const weights = await getWeights();
-  let attachment = '';
 
-  weights.forEach((element) => {
-    attachment += `${element.user.name} heeft ${Math.round(element.chance)}% kans om te gaan halen (C${element.chinees}/P${element.participant}/W${element.weight})\n`;
-  });
+  const attachment = weights
+    .map((el) => `${el.user.name} heeft ${Math.round(el.chance)}% kans om te gaan halen (C${el.chinees}/P${el.participant}/W${el.weight})`)
+    .join('\n');
+
   const attachmentData = {
     mrkdwn_in: ['text', 'pretext'],
     text: attachment,
@@ -145,9 +145,9 @@ function broodjesMenu(event) {
 async function broodjesLijst(event) {
   const lijst = await createBroodjeslijstAttachment();
   if (lijst === null) {
-    return broodjesReaction('Er zijn vandaag nog geen broodjes besteld :disappointed:', event, null);
+    return broodjesReaction('Er is vandaag nog niets besteld :disappointed:', event, null);
   }
-  const message = `Broodjes ${moment().format('dddd DD MMMM')}\n`;
+  const message = `Bestelling ${moment().format('dddd DD MMMM')}\n`;
   return broodjesReaction(message, event, lijst);
 }
 
@@ -157,10 +157,10 @@ async function addBroodje(broodje, user, event) {
   let message = 'Geen idee wat ik moet doen nu ¯\\_(ツ)_/¯';
 
   if (besteldBroodje) {
-    message = `Broodje gewijzigd naar *broodje ${broodje}* voor _${user.real_name}_`;
+    message = `Bestelling gewijzigd naar * ${broodje}* voor _${user.real_name}_`;
     besteldBroodje.broodje = broodje;
   } else {
-    message = `*Broodje ${broodje}* toegevoegd voor _${user.real_name}_`;
+    message = `*Bestelling ${broodje}* toegevoegd voor _${user.real_name}_`;
     besteldBroodje = new Broodje({
       broodje,
       userName: user.real_name,
@@ -181,15 +181,15 @@ function help(event) {
   const message = 'Bestel een broodje *voor 11u*, de broodjes worden *om 12u gehaald* door een chinese vrijwilliger.\nDe volgende commands zijn beschikbaar:';
   let attachment = '';
 
-  attachment += '*betty broodje smos kaas hesp*: je bestelt een broodje smos kaas hesp. Je kan dit natuurlijk wijzigen naar jouw eigen voorkeur. Voer dit command nog eens uit om jouw bestelling te wijzigen.\n';
-  attachment += '*betty broodje delete/remove*: Verwijder jouw bestelling.\n';
-  attachment += '*betty broodje menu*: Welke broodjes beschikbaar zijn.\n';
-  attachment += '*betty broodje lijst*: De volledige lijst van bestellingen.\n';
-  attachment += '*betty broodje halen*: Betty bepaalt wie het broodje moet gaan halen.\n';
-  attachment += '*betty broodje halen ikke*: Geef jezelf op als vrijwilliger om de broodjes te halen.\n';
-  attachment += '*betty broodje halen reset*: Reset de toewijzing van de chinese vrijwilliger.\n';
-  attachment += '*betty broodje stats*: Bekijk de kansberekening wie het broodje vandaag haalt.\n';
-  attachment += '*betty broodje help*: Deze boodschap.\n';
+  attachment += '*betty bestel smos kaas hesp*: je bestelt een broodje smos kaas hesp. Je kan dit natuurlijk wijzigen naar jouw eigen voorkeur. Voer dit command nog eens uit om jouw bestelling te wijzigen.\n';
+  attachment += '*betty bestel delete/remove*: Verwijder jouw bestelling.\n';
+  attachment += '*betty bestel menu*: Welke broodjes beschikbaar zijn.\n';
+  attachment += '*betty bestel lijst*: De volledige lijst van bestellingen.\n';
+  attachment += '*betty bestel halen*: Betty bepaalt wie het broodje moet gaan halen.\n';
+  attachment += '*betty bestel halen ikke*: Geef jezelf op als vrijwilliger om de broodjes te halen.\n';
+  attachment += '*betty bestel halen reset*: Reset de toewijzing van de chinese vrijwilliger.\n';
+  attachment += '*betty bestel stats*: Bekijk de kansberekening wie het broodje vandaag haalt.\n';
+  attachment += '*betty bestel help*: Deze boodschap.\n';
   const attachmentData = {
     mrkdwn_in: ['text', 'pretext'],
     text: attachment,
@@ -204,10 +204,10 @@ async function deleteBroodje(user, event) {
   let message = 'Geen idee wat ik moet doen nu ¯\\_(ツ)_/¯';
 
   if (besteldBroodje) {
-    message = `Broodje verwijderd voor _${user.real_name}_`;
+    message = `Bestelling verwijderd voor _${user.real_name}_`;
     besteldBroodje.remove();
   } else {
-    message = `_${user.real_name}_ heeft geen broodje besteld`;
+    message = `_${user.real_name}_ heeft niets besteld`;
   }
   broodjesReaction(message, event, null);
 }
@@ -221,7 +221,7 @@ export default function handle(event) {
     .toLowerCase()
     .split(' ');
 
-  if (sentence[0] !== 'broodje' && sentence[0] !== 'broodjes') {
+  if (sentence[0] !== 'bestel') {
     return false;
   }
 
@@ -268,7 +268,7 @@ export default function handle(event) {
 schedule.scheduleJob('00 11 * * 1-5', async () => {
   const lijst = await createBroodjeslijstAttachment();
   const response = {
-    message: '<!here> Heeft iedereen een broodje besteld?',
+    message: '<!here> Heeft iedereen besteld?',
     channel: 'C03LXRAGP',
     attachments: lijst,
   };
